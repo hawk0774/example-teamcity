91cf1ee5b0b26b46d0aa12d5268276a96371f290
hawk0774 <149391365+hawk0774@users.noreply.github.com>
2025-11-18 21:21:17 +0300

    Update WelcomerTest.java

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -31,7 +31,7 @@ public class WelcomerTest {
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
 	@Test
-	public void welcomerSaysHunter(){
+	public void welcomerSayHunter(){
 		assertThat(welcomer.saysHunter(), containsString("hunter"));
 	}
 }
